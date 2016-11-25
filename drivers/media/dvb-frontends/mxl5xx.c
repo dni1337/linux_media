e61c9f0865313db1f180c262120fa1d0523f48ad
CrazyCat <crazycat69@narod.ru>
2016-11-25 02:07:38 +0200

    mxl5xx: Fix in tune() routine.

--- drivers/media/dvb-frontends/mxl5xx.c
+++ drivers/media/dvb-frontends/mxl5xx.c
@@ -641,12 +641,13 @@ static int tune(struct dvb_frontend *fe, bool re_tune,
 		if (r)
 			return r;
 	}
-	if (*status & FE_HAS_LOCK)
-		return 0;
 
 	r = read_status(fe, status);
 	if (r)
 		return r;
+
+	if (*status & FE_HAS_LOCK)
+		return 0;
 
 	return 0;
 }
